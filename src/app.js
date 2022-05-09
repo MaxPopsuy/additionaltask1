ed87ce71240ec8120e815d49b2c5c73fbe26a5cc
Max Popsuy <maxim09p@gmail.com>
2022-05-09 20:02:10 +0300

    at added

---
+++ src/app.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const path = require('path');
+const { nanoid } = require('nanoid');
+
+const app = express();
+
+const viewsPath = path.join(process.cwd(), 'views');
+app.set('views', viewsPath);
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({ extended: false }));
+
+let contacts = [];
+
+app.get('/', (req, res) => {
+  res.render('index', {
+    contacts,
+  });
+});
+
+app.get('/create', (req, res) => {
+  res.render('create');
+});
+
+app.post('/create', (req, res) => {
+  contacts.push({
+    ...req.body,
+    id: nanoid(),
+  });
+
+  res.redirect('/');
+});
+
+app.get('/delete/:contactId', (req, res) => {
+  const { contactId } = req.params;
+
+  const filteredContacts = contacts.filter((item) => item.id !== contactId);
+  contacts = filteredContacts;
+
+  res.redirect('/');
+});
+
+app.get('/contact/:contactId', (req, res) => {
+  const {contactId} = req.params;
+  const contact = contacts.find(item => item.id === contactId);
+  res.render('info', {
+      contact,
+  })
+})
+
+
+module.exports = app;
